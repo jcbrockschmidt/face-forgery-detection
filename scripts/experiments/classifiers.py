122e054816d6841d9dc282cf233d12ffde17b41e
Joshua Brockschmidt <joshuabrockschmidt@gmail.com>
2019-07-12 15:14:35 -0400

    Remove dependency on MesoNet repository
    Copied model initializations from https://github.com/DariusAf/MesoNet

--- scripts/experiments/classifiers.py
+++ scripts/experiments/classifiers.py
@@ -1,12 +1,12 @@
 import keras.backend as K
 from keras.applications import Xception as KerasXception
 from keras.initializers import glorot_uniform, zeros
-from keras.layers import Dense, Dropout, Flatten, LeakyReLU
+from keras.layers import BatchNormalization, Concatenate, Conv2D, Dense, Dropout, Flatten, Input, LeakyReLU, MaxPooling2D
 from keras.models import Model as Model
 from keras.optimizers import Adam
-import MesoNet.classifiers as mesonet_classifiers
 
 CLASS_MODES = {'binary', 'categorical'}
+IMG_SIZE = 256
 
 class Classifier:
     """
@@ -20,7 +20,7 @@ class Classifier:
         Args:
             path: Path to weights file.
         """
-        raise NotImplementedError()
+        self.model.load_weights(path)
 
     def save(self, path):
         """
@@ -30,12 +30,12 @@ class Classifier:
             path: Path to weights file.
         raise NotImplementedError()
         """
-        raise NotImplementedError()
+        self.model.load_weights(path)
 
     def fit_with_generator(self, generator, steps_per_epoch,
                            validation_data=None,
                            validation_steps=None,
-                           class_weights=None,
+                           class_weight=None,
                            epochs=1,
                            initial_epoch=0,
                            shuffle=True,
@@ -43,7 +43,14 @@ class Classifier:
         """
         See keras.engine.training.Model.fit_generator.
         """
-        raise NotImplementedError()
+        return self.model.fit_generator(generator, steps_per_epoch,
+                                        validation_data=validation_data,
+                                        validation_steps=validation_steps,
+                                        class_weight=class_weight,
+                                        epochs=epochs,
+                                        initial_epoch=initial_epoch,
+                                        shuffle=shuffle,
+                                        callbacks=callbacks)
 
     def evaluate_with_generator(self, generator):
         """
@@ -55,7 +62,9 @@ class Classifier:
         Returns:
             Performance metrics.
         """
-        raise NotImplementedError()
+        return self.model.evaluate_generator(generator=generator,
+                                             steps=len(generator),
+                                             verbose=1)
 
     def set_metrics(self, metrics):
         """
@@ -64,140 +73,141 @@ class Classifier:
         Args:
             metrics: Keras-compatible metrics.
         """
-        raise NotImplementedError()
+        self.model.compile(optimizer=self.optimizer,
+                           loss='mean_squared_error',
+                           metrics=metrics)
 
 class Meso1(Classifier):
     """
-    Wrapper for a Meso-1 model.
+    Meso-1 model, borrowed from https://github.com/DariusAf/MesoNet
     """
 
-    def __init__(self, learning_rate=0.001):
-        self.lr = learning_rate
-        self.model = mesonet_classifiers.Meso1(self.lr)
+    def __init__(self, learning_rate=0.001, dl_rate=1):
+        self.model = self._init_model(dl_rate)
         self.optimizer = Adam(lr=learning_rate)
-        self.model.model.compile(optimizer=self.optimizer,
-                                 loss='mean_squared_error',
-                                 metrics=['accuracy'])
+        self.model.compile(optimizer=self.optimizer,
+                           loss='mean_squared_error',
+                           metrics=['accuracy'])
 
-    def load(self, path):
-        self.model.load(path)
+    def _init_model(self, dl_rate):
+        x = Input(shape = (IMG_SIZE, IMG_SIZE, 3))
 
-    def save(self, path):
-        self.model.load(path)
+        x1 = Conv2D(16, (3, 3), dilation_rate = dl_rate, strides = 1, padding='same', activation = 'relu')(x)
+        x1 = Conv2D(4, (1, 1), padding='same', activation = 'relu')(x1)
+        x1 = BatchNormalization()(x1)
+        x1 = MaxPooling2D(pool_size=(8, 8), padding='same')(x1)
 
-    def fit_with_generator(self, generator, steps_per_epoch,
-                           validation_data=None,
-                           validation_steps=None,
-                           class_weight=None,
-                           epochs=1,
-                           initial_epoch=0,
-                           shuffle=True,
-                           callbacks=None):
+        y = Flatten()(x1)
+        y = Dropout(0.5)(y)
+        y = Dense(1, activation = 'sigmoid')(y)
 
-        self.model.model.fit_generator(generator, steps_per_epoch,
-                                       validation_data=validation_data,
-                                       validation_steps=validation_steps,
-                                       class_weight=class_weight,
-                                       epochs=epochs,
-                                       initial_epoch=initial_epoch,
-                                       shuffle=shuffle,
-                                       callbacks=callbacks)
-
-    def evaluate_with_generator(self, generator):
-        return self.model.model.evaluate_generator(generator=generator,
-                                                   steps=len(generator),
-                                                   verbose=1)
+        return Model(inputs = x, outputs = y)
 
 class Meso4(Classifier):
     """
-    Wrapper for a Meso-4 model.
+    Meso-4 model, borrowed from https://github.com/DariusAf/MesoNet
     """
 
     def __init__(self, learning_rate=0.001):
-        self.lr = learning_rate
-        self.model = mesonet_classifiers.Meso4(self.lr)
+        self.model = self._init_model()
         self.optimizer = Adam(lr=learning_rate)
-        self.model.model.compile(optimizer=self.optimizer,
-                                 loss='mean_squared_error',
-                                 metrics=['accuracy'])
+        self.model.compile(optimizer=self.optimizer,
+                           loss='mean_squared_error',
+                           metrics=['accuracy'])
 
-    def load(self, path):
-        self.model.load(path)
+    def _init_model(self):
+        x = Input(shape = (IMG_SIZE, IMG_SIZE, 3))
 
-    def save(self, path):
-        self.model.load(path)
+        x1 = Conv2D(8, (3, 3), padding='same', activation = 'relu')(x)
+        x1 = BatchNormalization()(x1)
+        x1 = MaxPooling2D(pool_size=(2, 2), padding='same')(x1)
 
-    def fit_with_generator(self, generator, steps_per_epoch,
-                           validation_data=None,
-                           validation_steps=None,
-                           class_weight=None,
-                           epochs=1,
-                           initial_epoch=0,
-                           shuffle=True,
-                           callbacks=None):
+        x2 = Conv2D(8, (5, 5), padding='same', activation = 'relu')(x1)
+        x2 = BatchNormalization()(x2)
+        x2 = MaxPooling2D(pool_size=(2, 2), padding='same')(x2)
 
-        return self.model.model.fit_generator(generator, steps_per_epoch,
-                                              validation_data=validation_data,
-                                              validation_steps=validation_steps,
-                                              class_weight=class_weight,
-                                              epochs=epochs,
-                                              initial_epoch=initial_epoch,
-                                              shuffle=shuffle,
-                                              callbacks=callbacks)
+        x3 = Conv2D(16, (5, 5), padding='same', activation = 'relu')(x2)
+        x3 = BatchNormalization()(x3)
+        x3 = MaxPooling2D(pool_size=(2, 2), padding='same')(x3)
 
-    def evaluate_with_generator(self, generator):
-        return self.model.model.evaluate_generator(generator=generator,
-                                                   steps=len(generator),
-                                                   verbose=1)
+        x4 = Conv2D(16, (5, 5), padding='same', activation = 'relu')(x3)
+        x4 = BatchNormalization()(x4)
+        x4 = MaxPooling2D(pool_size=(4, 4), padding='same')(x4)
+
+        y = Flatten()(x4)
+        y = Dropout(0.5)(y)
+        y = Dense(16)(y)
+        y = LeakyReLU(alpha=0.1)(y)
+        y = Dropout(0.5)(y)
+        y = Dense(1, activation = 'sigmoid')(y)
+
+        return Model(inputs = x, outputs = y)
 
 class MesoInception4(Classifier):
     """
-    Wrapper for a MesoInception-4 model.
+    MesoInception-4 model, borrowed from https://github.com/DariusAf/MesoNet
     """
 
     def __init__(self, learning_rate=0.001):
-        self.lr = learning_rate
-        self.model = mesonet_classifiers.MesoInception4(learning_rate=self.lr)
+        self.model = self._init_model()
         self.optimizer = Adam(lr=learning_rate)
-        self.model.model.compile(optimizer=self.optimizer,
+        self.model.compile(optimizer=self.optimizer,
                                  loss='mean_squared_error',
                                  metrics=['accuracy'])
 
-    def load(self, path):
-        self.model.load(path)
+    def InceptionLayer(self, a, b, c, d):
+        """
+        Creates an inception layer.
 
-    def save(self, path):
-        self.model.load(path)
+        Returns:
+            Keras-compatible inception layer.
+        """
+        def func(x):
+            x1 = Conv2D(a, (1, 1), padding='same', activation='relu')(x)
 
-    def fit_with_generator(self, generator, steps_per_epoch,
-                           validation_data=None,
-                           validation_steps=None,
-                           class_weight=None,
-                           epochs=1,
-                           initial_epoch=0,
-                           shuffle=True,
-                           callbacks=None):
+            x2 = Conv2D(b, (1, 1), padding='same', activation='relu')(x)
+            x2 = Conv2D(b, (3, 3), padding='same', activation='relu')(x2)
 
-        return self.model.model.fit_generator(generator, steps_per_epoch,
-                                              validation_data=validation_data,
-                                              validation_steps=validation_steps,
-                                              class_weight=class_weight,
-                                              epochs=epochs,
-                                              initial_epoch=initial_epoch,
-                                              shuffle=shuffle,
-                                              callbacks=callbacks)
+            x3 = Conv2D(c, (1, 1), padding='same', activation='relu')(x)
+            x3 = Conv2D(c, (3, 3), dilation_rate = 2, strides = 1, padding='same', activation='relu')(x3)
 
-    def evaluate_with_generator(self, generator):
-        return self.model.model.evaluate_generator(generator=generator,
-                                                   steps=len(generator),
-                                                   verbose=1)
+            x4 = Conv2D(d, (1, 1), padding='same', activation='relu')(x)
+            x4 = Conv2D(d, (3, 3), dilation_rate = 3, strides = 1, padding='same', activation='relu')(x4)
 
-    def set_metrics(self, metrics):
-        self.model.model.compile(optimizer=self.optimizer,
-                                 loss='mean_squared_error',
-                                 metrics=metrics)
+            y = Concatenate(axis = -1)([x1, x2, x3, x4])
 
-class MesoInc4Frozen16(Classifier):
+            return y
+        return func
+
+    def _init_model(self):
+        x = Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+
+        x1 = self.InceptionLayer(1, 4, 4, 2)(x)
+        x1 = BatchNormalization()(x1)
+        x1 = MaxPooling2D(pool_size=(2, 2), padding='same')(x1)
+
+        x2 = self.InceptionLayer(2, 4, 4, 2)(x1)
+        x2 = BatchNormalization()(x2)
+        x2 = MaxPooling2D(pool_size=(2, 2), padding='same')(x2)
+
+        x3 = Conv2D(16, (5, 5), padding='same', activation = 'relu')(x2)
+        x3 = BatchNormalization()(x3)
+        x3 = MaxPooling2D(pool_size=(2, 2), padding='same')(x3)
+
+        x4 = Conv2D(16, (5, 5), padding='same', activation = 'relu')(x3)
+        x4 = BatchNormalization()(x4)
+        x4 = MaxPooling2D(pool_size=(4, 4), padding='same')(x4)
+
+        y = Flatten()(x4)
+        y = Dropout(0.5)(y)
+        y = Dense(16)(y)
+        y = LeakyReLU(alpha=0.1)(y)
+        y = Dropout(0.5)(y)
+        y = Dense(1, activation = 'sigmoid')(y)
+
+        return Model(inputs = x, outputs = y)
+
+class MesoInc4Frozen16(MesoInception4):
     """
     A MesoInception-4 model where the convolutional layers are untrainable.
     Keeps the original classification layers.
@@ -207,7 +217,7 @@ class MesoInc4Frozen16(Classifier):
 
     def __init__(self, learning_rate=0.001):
         self.lr = learning_rate
-        self.model = mesonet_classifiers.MesoInception4(learning_rate=self.lr).model
+        self.model = self._init_model()
 
         # Freeze the convolutional layers.
         for layer in self.model.layers[:self.FREEZE_BOUND]:
@@ -216,40 +226,6 @@ class MesoInc4Frozen16(Classifier):
         self.model.compile(optimizer=self.optimizer,
                            loss='mean_squared_error',
                            metrics=['accuracy'])
-
-    def load(self, path):
-        self.model.load_weights(path)
-
-    def save(self, path):
-        self.model.save_weights(path)
-
-    def fit_with_generator(self, generator, steps_per_epoch,
-                           validation_data=None,
-                           validation_steps=None,
-                           class_weight=None,
-                           epochs=1,
-                           initial_epoch=0,
-                           shuffle=True,
-                           callbacks=None):
-
-        return self.model.fit_generator(generator, steps_per_epoch,
-                                        validation_data=validation_data,
-                                        validation_steps=validation_steps,
-                                        class_weight=class_weight,
-                                        epochs=epochs,
-                                        initial_epoch=initial_epoch,
-                                        shuffle=shuffle,
-                                        callbacks=callbacks)
-
-    def evaluate_with_generator(self, generator):
-        return self.model.evaluate_generator(generator=generator,
-                                             steps=len(generator),
-                                             verbose=1)
-
-    def set_metrics(self, metrics):
-        self.model.compile(optimizer=self.optimizer,
-                           loss='mean_squared_error',
-                           metrics=metrics)
 
     def load_transfer(self, path):
         """
@@ -315,40 +291,6 @@ class MesoInc4Frozen48(Classifier):
                            loss='mean_squared_error',
                            metrics=['accuracy'])
 
-    def load(self, path):
-        self.model.load_weights(path)
-
-    def save(self, path):
-        self.model.save_weights(path)
-
-    def fit_with_generator(self, generator, steps_per_epoch,
-                           validation_data=None,
-                           validation_steps=None,
-                           class_weight=None,
-                           epochs=1,
-                           initial_epoch=0,
-                           shuffle=True,
-                           callbacks=None):
-
-        return self.model.fit_generator(generator, steps_per_epoch,
-                                        validation_data=validation_data,
-                                        validation_steps=validation_steps,
-                                        class_weight=class_weight,
-                                        epochs=epochs,
-                                        initial_epoch=initial_epoch,
-                                        shuffle=shuffle,
-                                        callbacks=callbacks)
-
-    def evaluate_with_generator(self, generator):
-        return self.model.evaluate_generator(generator=generator,
-                                             steps=len(generator),
-                                             verbose=1)
-
-    def set_metrics(self, metrics):
-        self.model.compile(optimizer=self.optimizer,
-                           loss='mean_squared_error',
-                           metrics=metrics)
-
     def load_transfer(self, path):
         """
         Loads weights from a MesoInception-4 model.
@@ -386,7 +328,7 @@ class Xception(Classifier):
     def __init__(self, learning_rate=0.001):
         self.lr = learning_rate
         self.optimizer = Adam(lr=learning_rate)
-        self.model = KerasXception(weights=None, input_shape=(256, 256, 3),
+        self.model = KerasXception(weights=None, input_shape=(IMG_SIZE, IMG_SIZE, 3),
                                    include_top=False, pooling='avg')
         y = Dense(1, activation='sigmoid', name='prediction')(
             self.model.layers[-1].output)
@@ -395,52 +337,6 @@ class Xception(Classifier):
         self.model.compile(optimizer=self.optimizer,
                            loss='mean_squared_error',
                            metrics=['accuracy'])
-
-    def load(self, path):
-        """
-        Load weights for a model.
-
-        Args:
-            path: Path to weights file.
-        """
-        self.model.load_weights(path)
-
-    def save(self, path):
-        """
-        Save weights for a model.
-
-        Args:
-            path: Path to weights file.
-        """
-        self.model.save_weights(path)
-
-    def fit_with_generator(self, generator, steps_per_epoch,
-                           validation_data=None,
-                           validation_steps=None,
-                           class_weight=None,
-                           epochs=1,
-                           initial_epoch=0,
-                           shuffle=True,
-                           callbacks=None):
-
-        return self.model.fit_generator(generator, steps_per_epoch,
-                                        validation_data=validation_data,
-                                        validation_steps=validation_steps,
-                                        class_weight=class_weight,
-                                        epochs=epochs,
-                                        initial_epoch=initial_epoch,
-                                        shuffle=shuffle,
-                                        callbacks=callbacks)
-
-    def evaluate_with_generator(self, generator):
-        return self.model.evaluate_generator(generator=generator,
-                                             steps=len(generator),
-                                             verbose=1)
-
-    def set_metrics(self, metrics):
-        self.model.compile(optimizer=self.optimizer,
-                           loss='mean_squared_error',
-                           metrics=metrics)
 
 MODEL_MAP = {
     'meso1': Meso1,
