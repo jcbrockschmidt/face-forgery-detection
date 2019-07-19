3d571cc5e2be0e91b0a94740442ac3643e6ead66
Joshua Brockschmidt <joshuabrockschmidt@gmail.com>
2019-07-19 16:11:29 -0400

    Do not show results for models trained on all compression levels

---
+++ scripts/visualize/common.py
@@ -0,0 +1,17 @@
+CLASS_TO_COLOR = {
+    'f2f' : (0, 1, 0),      # Green
+    'df'  : (1, 0, 0),      # Red
+    'fs'  : (0, 1, 1),      # Cyan
+    'icf' : (1, 0.6, 0),    # Orange
+    'gann': (1, 0.7, 0.8),  # Pink
+    'x2f' : (0, 0, 1)       # Blue
+}
+
+CLASS_TO_LABEL = {
+    'f2f'  : 'Face2Face',
+    'df'   : 'Deepfakes',
+    'fs'   : 'FaceSwap',
+    'icf'  : 'ICface',
+    'gann' : 'GANnotation',
+    'x2f'  : 'X2Face'
+}
