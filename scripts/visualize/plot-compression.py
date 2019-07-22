adf5fbc9c6df77a9123f85ea3d3f6f09697a49ec
Joshua Brockschmidt <joshuabrockschmidt@gmail.com>
2019-07-22 11:03:49 -0400

    Add plot for accuracies of models trained on all compression levels

--- scripts/visualize/plot-compression.py
+++ scripts/visualize/plot-compression.py
@@ -10,6 +10,7 @@ import csv
 from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter, FuncFormatter, MaxNLocator
+import numpy as np
 import os
 from sys import stderr
 
@@ -28,20 +29,24 @@ HINDEX = {v: i for i, v in enumerate(HEADERS)}
 CLASSES = ('f2f', 'df', 'fs', 'icf', 'gann', 'x2f')
 
 COMP_TO_LINESTYLE = {
-    'c0'  : '-',
-    'c23' : '--',
-    'c40' : ':'
+    'c0'  : '--',
+    'c23' : '-.',
+    'c40' : ':',
+    'all' : '-'
 }
 
 COMP_TO_LABEL = {
     'c0'  : 'Trained on lossless',
     'c23' : 'Trained on visibly lossless',
-    'c40' : 'Trained on lossy'
+    'c40' : 'Trained on lossy',
+    'all' : 'Trained on all'
 }
 
 COMP_LEVELS = ('c0', 'c23', 'c40')
 
 X_LABELS = ('Lossless', 'Visibly lossless', 'Lossy')
+
+GUIDE_COLOR = (0.9, 0.9, 0.9)
 
 def x_to_comp(tick_val, tick_pos):
     """
@@ -82,7 +87,7 @@ def load_model_data(csv_path):
                 models[mtype][class_type] = {}
 
             test_results = {}
-            for test_comp in COMP_LEVELS:
+            for test_comp in list(COMP_LEVELS) + ['all']:
                 res = {}
                 for metric in ('acc', 'tpr', 'tnr'):
                     label = '{}_{}'.format(metric, test_comp)
@@ -109,7 +114,7 @@ def plot_accuracy(models):
     ax.set_ylabel('Accuracy')
     ax.xaxis.set_major_formatter(FuncFormatter(x_to_comp))
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    xs = (0, 1, 2)
+    xs = np.arange(len(COMP_LEVELS))
     leg_handles = []
     leg_labels = []
     for mtype in models:
@@ -118,7 +123,7 @@ def plot_accuracy(models):
             if class_type not in model:
                 continue
             color = CLASS_TO_COLOR[class_type]
-            for train_comp in COMP_LEVELS:
+            for train_comp in list(COMP_LEVELS) + ['all']:
                 res = model[class_type][train_comp]
                 label = '{}, {}'.format(class_type, train_comp)
                 ys = (res['c0']['acc'], res['c23']['acc'], res['c40']['acc'])
@@ -132,8 +137,10 @@ def plot_accuracy(models):
             leg_labels.append(CLASS_TO_LABEL[class_type])
 
     # Draw lines marking each compression level.
-    for x in xs:
-        plt.axvline(x=x, linestyle='-', color=(0, 0, 0, 0.1))
+
+    # Add guidelines for the X ticks.
+    ax.set_axisbelow(True)
+    ax.xaxis.grid(color=GUIDE_COLOR, linestyle='-')
 
     # Add legend entries for line styles.
     for comp in COMP_TO_LINESTYLE:
@@ -166,7 +173,7 @@ def plot_tpr_vs_tnr(models):
     ax.tick_params(axis='y', which='major')
     ax.yaxis.set_minor_formatter(FormatStrFormatter('%.1f'))
     ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
-    xs = (0, 1, 2)
+    xs = np.arange(len(COMP_LEVELS))
     leg_handles = []
     leg_labels = []
     for mtype in models:
@@ -175,7 +182,7 @@ def plot_tpr_vs_tnr(models):
             if class_type not in model:
                 continue
             color = CLASS_TO_COLOR[class_type]
-            for train_comp in COMP_LEVELS:
+            for train_comp in list(COMP_LEVELS) + ['all']:
                 res = model[class_type][train_comp]
                 tprs = (res['c0']['tpr'], res['c23']['tpr'], res['c40']['tpr'])
                 tnrs = (res['c0']['tnr'], res['c23']['tnr'], res['c40']['tnr'])
@@ -188,10 +195,18 @@ def plot_tpr_vs_tnr(models):
             leg_handles.append(Line2D([], [], linestyle='-', color=color))
             leg_labels.append(CLASS_TO_LABEL[class_type])
 
-    # Draw lines marking each compression level and the ideal ratio.
+    # Add guidelines for the X ticks.
+    ax.set_axisbelow(True)
+    ax.xaxis.grid(color=GUIDE_COLOR, linestyle='-')
+
+    # Add a guideline at y = 1.
+    ax.set_axisbelow(True)
+    plt.axhline(y=1, linestyle='-', color=GUIDE_COLOR)
+    '''
     for x in xs:
-        plt.axvline(x=x, linestyle='-', color=(0, 0, 0, 0.1))
-    plt.axhline(y=1, linestyle='-', color=(0, 0, 0, 0.1))
+        plt.axvline(x=x, linestyle='-', color=GUIDE_COLOR)
+    plt.axhline(y=1, linestyle='-', color=GUIDE_COLOR)
+    '''
 
     # Add legend entries for line styles.
     for comp in COMP_TO_LINESTYLE:
@@ -207,6 +222,61 @@ def plot_tpr_vs_tnr(models):
 
     return fig, ax
 
+def plot_combined(models):
+    """
+    Plots a bar graph with accuracies for models trained on all compression levels.
+
+    Args:
+        models: A dictionary returned by `load_model_data`.
+
+    Returns:
+        Matplotlib figure and axes.
+    """
+    BAR_WIDTH = 0.2
+    METRICS = ('acc', 'tpr', 'tnr')
+    METRIC_TO_COLOR = {
+        'acc' : (0.5, 0, 0.5),  # Purple
+        'tpr' : (1, 0, 0),      # Red
+        'tnr' : (0, 0, 1)       # Blue
+    }
+    METRIC_TO_LABEL = {
+        'acc' : 'Accuracy',
+        'tpr' : 'Real recall',
+        'tnr' : 'Fake recall'
+    }
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Class')
+    ax.set_ylabel('Score')
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+
+    y_min = 1.0
+    xs = np.arange(len(CLASSES)) - BAR_WIDTH
+    for mtype in models:
+        for i, metric in enumerate(METRICS):
+            ys = []
+            for c in CLASSES:
+                res = models[mtype][c]['all']['all']
+                m = float(res[metric])
+                ys.append(m)
+                y_min = min(y_min, m)
+            ax.bar(xs + (BAR_WIDTH * i), ys, BAR_WIDTH,
+                   color=METRIC_TO_COLOR[metric],
+                   label=METRIC_TO_LABEL[metric])
+
+    x_labels = [CLASS_TO_LABEL[c] for c in CLASSES]
+    ax.set_xticks(np.arange(len(CLASSES)))
+    ax.set_xticklabels(x_labels)
+    ax.set_ylim((y_min - 0.05, 1.005))
+
+    # Add guidelines for the Y ticks.
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(color=GUIDE_COLOR, linestyle='-')
+
+    ax.legend()
+
+    return fig, ax
+
 def main(csv_path):
     """
     Creates and displays various plots using the supplied experimentation data.
@@ -214,10 +284,14 @@ def main(csv_path):
     models = load_model_data(csv_path)
 
     # Plot accuracies against each compression level.
-    acc_fig, acc_ax = plot_accuracy(models)
+    plot_accuracy(models)
 
     # Plot TPR vs. TNR of models against each compression level.
-    vs_fig, vs_ax = plot_tpr_vs_tnr(models)
+    plot_tpr_vs_tnr(models)
+
+    # Plot accuracy, TPR, and TNR for models trained on all compression levels.
+    plot_combined(models)
+
     plt.show()
 
 if __name__ == '__main__':
